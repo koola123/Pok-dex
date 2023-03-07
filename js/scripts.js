e3b9915a7d949c72ecd6efae8258b25e3a895d52
Matthias Müringer <mmueringer@web.de>
2023-03-07 23:47:02 +0100

    Created new functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,56 @@
+  // Wrap pokemonlist into an IIFE
+let pokemonRepository = (function() {
+// Proteced pokemonlist inside IIFE
+  let pokemonList = [
+    {
+      name: "Bulbasaur",
+      height: 1.7,
+      types: ["Grass, Ground"]
+    },
+    {
+      name: "Shiggy",
+      height: 1.3,
+      types: ["Water, Fontaine"]
+    },
+    {
+      name: "Mewtoo",
+      height: 2.0,
+      types: ["Psycho, Fire"]
+    },
+    {
+      name: "Kokuna",
+      height: 1.2,
+      types: ["Wood"]
+    },
+    {
+      name: "Smettbo",
+      height: 3,
+      types: ["Wind, Flower"]
+    }
+  ];
 
-// Wrap pokemonlist into an IIFE
-let pokemonList = (function() {
-  {
-    name: "Bulbasaur",
-    height: 1.7,
-    types: ["Grass, Ground"]
-  },
-  {
-    name: "Shiggy",
-    height: 1.3,
-    types: ["Water, Fontaine"]
-  },
-  {
-    name: "Mewtoo",
-    height: 2.0,
-    types: ["Psycho, Fire"]
-  },
-  {
-    name: "Kokuna",
-    height: 1.2,
-    types: ["Wood"]
-  },
-  {
-    name: "Smettbo",
-    height: 3,
-    types: ["Wind, Flower"]
+// Function to print out the pokemonlist outside the IIFE
+  function getAll() {
+    return pokemonList;
   }
-];
+
+// Function to add a new pokemon object to the pokemonlist array
+  function add(item) {
+    pokemonList.push(item);
+  }
+// Return both functions from pokemonRepository IIFE
+  return {
+    getAll: getAll,
+    add: add
+  }
+
 })();
 
 
+// Add a new pokemon object to the pokemonlist
+pokemonRepository.add({name: "Charmander", height: 10, types: ["Fire", "Volcano"]})
 
 // ForEach() Loop to print out the pokemonList
-pokemonList.forEach(function(item) {
+pokemonRepository.getAll().forEach(function(item) {
   document.write("<div>" + item.name + " " + item.height + " " +  item.types + " " + "</div>")
 })
-
-//
-// // For loop to print out the pokemonlist to the DOM by using the document.write built-in function
-// for (let i=0; i< pokemonList.length; i++) {
-// // If else statement to highlight and print out the pokemon with the tallest height
-//   if (pokemonList[i].height > 2.0) {
-//     document.write( "<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height +")" + ' - Wow, that\'s big!' + "</div>")
-//   } else {
-//     document.write("<div>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height +")" + "</div>");
-//   }
-// }
